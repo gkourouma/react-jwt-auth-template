18c7fab2a3eb50516ad6ae4204ee20053f2be377
gkourouma <gkourouma2@gmail.com>
2025-04-03 13:59:42 -0500

    deleted extra index.css import

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router"; // add import for BrowserRouter
 import { UserProvider } from "./contexts/UserContext.jsx";
 import "./index.css";
 import App from "./App.jsx";
-import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
