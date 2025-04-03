01936bb105c1b7485d65c8ebd4edf891b9691bbe
gkourouma <gkourouma2@gmail.com>
2025-04-03 12:43:28 -0500

    wip: imported UserProvider and index.css file. Wrapped <app> in UserProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router"; // add import for BrowserRouter
+import { UserProvider } from "./contexts/UserContext.jsx";
 import "./index.css";
 import App from "./App.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </StrictMode>
 );
