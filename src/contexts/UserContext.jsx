59a7b336df08482ec0305a5d79272c3ac6f6c56b
gkourouma <gkourouma2@gmail.com>
2025-04-03 12:44:15 -0500

    wip: created usercontent.jsx

---
+++ src/contexts/UserContext.jsx
@@ -0,0 +1,15 @@
+import { createContext } from 'react';
+
+const UserContext = createContext();
+
+function UserProvider({ children }) {
+
+  return (
+    <UserContext.Provider>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export { UserProvider };
+
