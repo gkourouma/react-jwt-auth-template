0aaf0538b2f7353d97780ccf23b9024cd388ea6e
gkourouma <gkourouma2@gmail.com>
2025-04-03 13:59:20 -0500

    wip: imported sign in form and added the sign in route

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import { Routes, Route } from 'react-router'; // Import React Router
 import NavBar from './components/NavBar/NavBar';
 // Import the SignUpForm component
 import SignUpForm from './components/SignUpForm/SignUpForm';
+import SignInForm from './components/SignInForm/SignInForm';
 
 const App = () => {
 
   return (
     <>
       <NavBar />
-      {/* Add the Routes component to wrap our individual routes*/}
       <Routes>
         <Route path='/sign-up' element={<SignUpForm />} />
+        <Route path="/sign-in" element={<SignInForm />} />
       </Routes>
     </>
   );
