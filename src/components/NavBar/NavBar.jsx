6bf0ff8e50729108410fa98cd0b69d29dfba3633
gkourouma <gkourouma2@gmail.com>
2025-04-03 14:00:52 -0500

    imported useContext and userContext, added sign in link to navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,35 @@
-import { Link } from 'react-router';
+import { useContext } from "react";
+import { Link } from "react-router";
+import { UserContext } from "../../contexts/UserContext";
+
 const NavBar = () => {
-    return (
-      <nav>
+  const { user } = useContext(UserContext);
+
+  // Add the handleSignOut function
+  const handleSignOut = () => {
+    // Clear the token from localStorage
+    localStorage.removeItem('token');
+    // Clear the user state
+    setUser(null);
+  }
+
+  return (
+    <nav>
+      {user ? (
         <ul>
-        <li><Link to='/sign-up'>Sign Up</Link></li>
-      </ul>
-      </nav>
-    );
-  };
-  
-  export default NavBar;
-  
-  
+          <li>Welcome, {user.username}</li>
+          <li><Link to='/'>Sign Out</Link></li>
+        </ul>
+      ) : (
+        <ul>
+          <li>
+            <Link to="/sign-up">Sign Up</Link>
+            <Link to="/sign-in">Sign In</Link>
+          </li>
+        </ul>
+      )}
+    </nav>
+  );
+};
+
+export default NavBar;
